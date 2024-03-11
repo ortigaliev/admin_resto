9968553f047bb8cae1ccc6e69e0b8fb49d5314e9
ortigaliev <ortigaliyevm@gmail.com>
2024-03-12 08:03:27 +0900

    feat: Develop order Rest Api business logic  creation part one

---
+++ controllers/orderController.js
@@ -0,0 +1,21 @@
+const assert = require("assert");
+const Order = require("../models/Order");
+
+let orderController = module.exports;
+const jwt = require("jsonwebtoken");
+const Definer = require("../lib/mistakes");
+
+orderController.createOrder = async (req, res) => {
+  try {
+    console.log(`POST: cont/createOrder`);
+    assert.ok(req.member, Definer.auth_err5);
+
+    const order = new Order();
+    const result = await order.createOrderData(req.member, req.body);
+
+    res.json({ state: "success", data: result });
+  } catch (err) {
+    console.log(`ERROR, cont/createOrder, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
+};
--- router.js
+++ router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const memberController = require("./controllers/memberController");
 const productController = require("./controllers/productController");
 const restaurantController = require("./controllers/restaurantController");
+const orderController = require("./controllers/orderController");
 
 //Member realted routers
 router.post("/signup", memberController.signup);
@@ -38,6 +39,13 @@ router.get(
   "/restaurants/:id",
   memberController.retrieveAuthMember,
   restaurantController.getChosenRestaurant
+);
+
+//Order related routers
+router.post(
+  "/orders/create",
+  memberController.retrieveAuthMember,
+  orderController.createOrder
 );
 
 //Others
