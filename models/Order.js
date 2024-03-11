6083a61b40fe0d1ef403ca3057ae5174700f5b23
ortigaliev <ortigaliyevm@gmail.com>
2024-03-12 08:17:26 +0900

    feat: Complete order & orderItems Rest API business logic

--- models/Order.js
+++ models/Order.js
@@ -32,6 +32,10 @@ class Order {
       );
       console.log("order_id:::", order_id);
       //order items creation
+
+      //order items creation
+
+      await this.recordOrderItemsData(order_id, data);
       return order_id;
     } catch (err) {
       throw err;
@@ -52,6 +56,40 @@ class Order {
     } catch (err) {
       console.log(err);
       throw new Error(Definer.order_err1);
+    }
+  }
+  async recordOrderItemsData(order_id, data) {
+    try {
+      const pro_list = data.map(async (item) => {
+        return await this.saveOrderItemsData(item, order_id);
+      });
+
+      const results = await Promise.all(pro_list);
+      console.log("results:::", results);
+      return true;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async saveOrderItemsData(item, order_id) {
+    try {
+      order_id = shapeIntoMongooseObjectId(order_id);
+      item._id = shapeIntoMongooseObjectId(item._id);
+
+      const order_item = new this.orderItemModel({
+        item_quantity: item["quantity"],
+        item_price: item["price"],
+        order_id: order_id,
+        product_id: item["_id"],
+      });
+      const result = await order_item.save();
+      assert.ok(result, Definer.order_err2);
+
+      return "inserted";
+    } catch (err) {
+      console.log(err);
+      throw new Error(Definer.order_err2);
     }
   }
 }
