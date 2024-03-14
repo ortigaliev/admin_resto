4a0bdaee3bf2ecf9fc4189222c39299f3bd185f3
ortigaliev <ortigaliyevm@gmail.com>
2024-03-14 17:13:48 +0900

    feat: Develop getMemberFollowers and follow back Rest API Business logic

--- router.js
+++ router.js
@@ -102,4 +102,10 @@ router.post(
 );
 router.get("/follow/followings", followController.getMemberFollowings);
 
+router.get(
+  "/follow/followers",
+  memberController.retrieveAuthMember,
+  followController.getMemberFollowers
+);
+
 module.exports = router;
