aaf40ca208cb45139b41dddcfcfd158f13bab0c9
ortigaliev <ortigaliyevm@gmail.com>
2024-03-14 11:30:26 +0900

    feat: Develop subscription Rest API  business logic

---
+++ controllers/followController.js
@@ -0,0 +1,20 @@
+const assert = require("assert");
+const Definer = require("../lib/mistakes");
+const Follow = require("../models/Follow");
+
+let followController = module.exports;
+
+followController.subscribe = async (req, res) => {
+  try {
+    console.log(`POST: cont/subscribe`);
+    assert.ok(req.member, Definer.auth_err5);
+
+    const follow = new Follow();
+    await follow.subscribeData(req.member, req.body);
+
+    res.json({ state: "success", data: "Subscribed" });
+  } catch (err) {
+    console.log(`ERROR, cont/subscribe, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
+};
